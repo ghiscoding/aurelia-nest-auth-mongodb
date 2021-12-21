49842e9911adff74a34603a0a02e16353dfa8dc3
ghiscoding <gbeaulac@gmail.com>
2021-12-20 21:03:37 -0500

    chore(deps): update npm Aurelia-Slickgrid 4 and WebPack 5

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,3 @@
-/// <reference types="aurelia-loader-webpack/src/webpack-hot-interface"/>
-
 // we want font-awesome to load as soon as possible to show the fa-spinner
 import 'font-awesome/css/font-awesome.css';
 import 'nprogress/nprogress.css';
@@ -14,22 +12,12 @@ import '@slickgrid-universal/common/dist/styles/css/slickgrid-theme-bootstrap.cs
 import { Aurelia } from 'aurelia-framework';
 import { PLATFORM } from 'aurelia-pal';
 import { GridOption } from 'aurelia-slickgrid';
-import * as Bluebird from 'bluebird';
 import 'bootstrap';
 
 import './styles/styles.scss';
 import './styles/bootstrap-social.scss';
-import * as environment from '../config/environment.json';
 
-/** Disable long stack traces for IE11 */
-Bluebird.config({
-  warnings: {
-    wForgottenReturn: false
-  },
-  longStackTraces: false
-});
-
-export function configure(aurelia: Aurelia) {
+export async function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .feature(PLATFORM.moduleName('resources/index'));
@@ -40,13 +28,6 @@ export function configure(aurelia: Aurelia) {
     // config.options.gridMenu.iconCssClass = 'fa fa-ellipsis-v'
   });
 
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
-
-  if (environment.testing) {
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
-  }
-
-  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+  await aurelia.start();
+  await aurelia.setRoot(PLATFORM.moduleName('app'));
 }
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,6 +8,7 @@ const { AureliaPlugin, ModuleDependenciesPlugin } = require('aurelia-webpack-plu
 const { ProvidePlugin } = require('webpack');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 // config helpers:
 const ensureArray = (config) => config && (Array.isArray(config) ? config : [config]) || [];
@@ -16,16 +17,13 @@ const when = (condition, config, negativeConfig) =>
 
 // primary config:
 const title = 'Aurelia Navigation Skeleton';
-const outDir = path.resolve(__dirname, project.platform.output);
+const outDevDir = path.resolve(__dirname, project.platform.output);
+const outProdDir = path.resolve(__dirname, project.platform.outputProd);
+const nodeModulesDir = path.resolve(__dirname, 'node_modules');
 const srcDir = path.resolve(__dirname, 'src');
 const baseUrl = '';
 
-const cssRules = [
-  { loader: 'css-loader' },
-];
-
-
-module.exports = ({ production } = {}, { extractCss, analyze, tests, hmr, port, host } = {}) => ({
+module.exports = ({ production } = {}, { analyze, server, tests } = {}) => ({
   resolve: {
     extensions: ['.ts', '.js'],
     modules: [srcDir, 'node_modules'],
@@ -35,145 +33,71 @@ module.exports = ({ production } = {}, { extractCss, analyze, tests, hmr, port, 
       'aurelia-binding': path.resolve(__dirname, 'node_modules/aurelia-binding'),
       'jquery': path.join(__dirname, 'node_modules/jquery/dist/jquery'),
       moment$: 'moment/moment.js'
+    },
+    fallback: {
+      http: false,
+      https: false,
+      stream: false,
+      util: false,
+      zlib: false,
     }
   },
   entry: {
     app: ['aurelia-bootstrapper'],
-    vendor: ['bluebird', 'jquery']
+    vendor: ['jquery']
   },
   mode: production ? 'production' : 'development',
   output: {
-    path: outDir,
+    path: production ? outProdDir : outDevDir,
     publicPath: baseUrl,
-    filename: production ? '[name].[chunkhash].bundle.js' : '[name].[hash].bundle.js',
-    sourceMapFilename: production ? '[name].[chunkhash].bundle.map' : '[name].[hash].bundle.map',
-    chunkFilename: production ? '[name].[chunkhash].chunk.js' : '[name].[hash].chunk.js'
+    filename: '[name].[contenthash].bundle.js',
+    sourceMapFilename: '[name].[contenthash].bundle.js.map',
+    chunkFilename: '[name].[contenthash].chunk.js',
   },
-  optimization: {
-    runtimeChunk: true,  // separates the runtime chunk, required for long term cacheability
-    // moduleIds is the replacement for HashedModuleIdsPlugin and NamedModulesPlugin deprecated in https://github.com/webpack/webpack/releases/tag/v4.16.0
-    // changes module id's to use hashes be based on the relative path of the module, required for long term cacheability
-    moduleIds: 'hashed',
-    // Use splitChunks to breakdown the App/Aurelia bundle down into smaller chunks
-    // https://webpack.js.org/plugins/split-chunks-plugin/
-    splitChunks: {
-      hidePathInfo: true, // prevents the path from being used in the filename when using maxSize
-      chunks: "initial",
-      // sizes are compared against source before minification
-      maxSize: 500000, // splits chunks if bigger than 500k, adjust as required (maxSize added in webpack v4.15)
-      cacheGroups: {
-        default: false, // Disable the built-in groups default & vendors (vendors is redefined below)
-        // You can insert additional cacheGroup entries here if you want to split out specific modules
-        // This is required in order to split out vendor css from the app css when using --extractCss
-        // For example to separate font-awesome and bootstrap:
-        fontawesome: { // separates font-awesome css from the app css (font-awesome is only css/fonts)
-          name: 'vendor.font-awesome',
-          test: /[\\/]node_modules[\\/]font-awesome[\\/]/,
-          priority: 100,
-          enforce: true
-        },
-        // bootstrap: { // separates bootstrap js from vendors and also bootstrap css from app css
-        //   name: 'vendor.font-awesome',
-        //   test: /[\\/]node_modules[\\/]bootstrap[\\/]/,
-        //   priority: 90,
-        //   enforce: true
-        // },
-
-        // This is the HTTP/1.1 optimised cacheGroup configuration
-        vendors: { // picks up everything from node_modules as long as the sum of node modules is larger than minSize
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          priority: 19,
-          enforce: true, // causes maxInitialRequests to be ignored, minSize still respected if specified in cacheGroup
-          minSize: 30000 // use the default minSize
-        },
-        vendorsAsync: { // vendors async chunk, remaining asynchronously used node modules as single chunk file
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors.async',
-          chunks: 'async',
-          priority: 9,
-          reuseExistingChunk: true,
-          minSize: 20000  // use smaller minSize to avoid too much potential bundle bloat due to module duplication.
-        },
-        commonsAsync: { // commons async chunk, remaining asynchronously used modules as single chunk file
-          name: 'commons.async',
-          minChunks: 2, // Minimum number of chunks that must share a module before splitting
-          chunks: 'async',
-          priority: 0,
-          reuseExistingChunk: true,
-          minSize: 20000  // use smaller minSize to avoid too much potential bundle bloat due to module duplication.
-        }
-      }
-    }
+  stats: {
+    warnings: false
   },
+  target: 'web',
   performance: { hints: false },
   devServer: {
-    contentBase: outDir,
+    static: {
+      directory: production ? outProdDir : outDevDir,
+    },
     // serve index.html for all 404 (required for push-state)
-    historyApiFallback: true,
-    hot: hmr || project.platform.hmr,
-    port: port || project.platform.port,
-    host: host || project.platform.host,
+    historyApiFallback: project.platform.historyApiFallback,
+    compress: project.platform.compress,
+    liveReload: project.platform.liveReload,
+    hot: project.platform.hmr,
+    port: project.platform.port,
+    host: project.platform.host,
     open: project.platform.open,
   },
-  devtool: production ? 'nosources-source-map' : 'cheap-module-eval-source-map',
+  devtool: production ? false : 'eval-cheap-module-source-map',
   module: {
     rules: [
       // CSS required in JS/TS files should use the style-loader that auto-injects it into the website
       // only when the issuer is a .js/.ts file, so the loaders are not applied inside html templates
       {
         test: /\.css$/i,
-        issuer: [{ not: [{ test: /\.html$/i }] }],
-        use: extractCss ? [{
-          loader: MiniCssExtractPlugin.loader
-        },
-          'css-loader'
-        ] : ['style-loader', ...cssRules]
+        use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader']
       },
-      {
-        test: /\.css$/i,
-        issuer: [{ test: /\.html$/i }],
-        // CSS required in templates cannot be extracted safely
-        // because Aurelia would try to require it again in runtime
-        use: cssRules
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        issuer: /\.[tj]s$/i
-      },
-      {
-        test: /\.scss$/,
-        use: ['css-loader', 'sass-loader'],
-        issuer: /\.html?$/i
-      },
+      { test: /\.(sass|scss)$/, use: ['style-loader', 'css-loader', 'sass-loader'], issuer: /\.[tj]s$/i },
+      { test: /\.(sass|scss)$/, use: ['css-loader', 'sass-loader'], issuer: /\.html?$/i },
       { test: /\.html$/i, loader: 'html-loader' },
-      { test: /\.ts$/, loader: "ts-loader" },
-      // exposes jQuery globally as $ and as jQuery:
-      // { test: require.resolve('jquery'), loader: 'expose-loader?$!expose-loader?jQuery' },
-      // embed small images and fonts as Data Urls and larger ones as files:
-      { test: /\.(png|gif|jpg|cur)$/i, loader: 'url-loader', options: { limit: 8192 } },
-      { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff2' } },
-      { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff' } },
-      // load these fonts normally, as files:
-      { test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'file-loader' },
-      {
-        test: /environment\.json$/i, use: [
-          { loader: "app-settings-loader", options: { env: production ? 'production' : 'development' } },
-        ]
-      },
-      ...when(tests, {
-        test: /\.[jt]s$/i, loader: 'istanbul-instrumenter-loader',
-        include: srcDir, exclude: [/\.(spec|test)\.[jt]s$/i],
-        enforce: 'post', options: { esModules: true },
-      })
+      { test: /\.ts?$/, use: 'ts-loader', exclude: nodeModulesDir, },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource', },
+      { test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i, type: 'asset/resource', },
     ]
   },
   plugins: [
     ...when(!tests, new DuplicatePackageCheckerPlugin()),
-    new AureliaPlugin(),
+    new AureliaPlugin({
+      // dist: 'es2015',
+      // aureliaApp: 'main',
+      // aureliaConfig: ['basic'],
+      // includeAll: 'src'
+    }),
     new ProvidePlugin({
-      'Promise': 'bluebird',
       '$': 'jquery',
       'jQuery': 'jquery',
       'window.jQuery': 'jquery',
@@ -184,16 +108,17 @@ module.exports = ({ production } = {}, { extractCss, analyze, tests, hmr, port, 
     }),
     new HtmlWebpackPlugin({
       template: 'index.ejs',
+      favicon: `${srcDir}/favicon.ico`,
       metadata: {
         // available in index.ejs //
-        title, baseUrl
+        title, server, baseUrl
       }
     }),
     // ref: https://webpack.js.org/plugins/mini-css-extract-plugin/
-    ...when(extractCss, new MiniCssExtractPlugin({ // updated to match the naming conventions for the js files
-      filename: production ? '[name].[contenthash].bundle.css' : '[name].[hash].bundle.css',
-      chunkFilename: production ? '[name].[contenthash].chunk.css' : '[name].[hash].chunk.css'
-    })),
+    new MiniCssExtractPlugin({ // updated to match the naming conventions for the js files
+      filename: '[name].[contenthash].bundle.css',
+      chunkFilename: '[name].[contenthash].chunk.css'
+    }),
     new CopyWebpackPlugin({
       patterns: [
         { from: `${srcDir}/favicon.ico`, to: 'favicon.ico' },
@@ -201,12 +126,8 @@ module.exports = ({ production } = {}, { extractCss, analyze, tests, hmr, port, 
       ]
     }),
     ...when(analyze, new BundleAnalyzerPlugin()),
-    /**
-     * Note that the usage of following plugin cleans the webpack output directory before build.
-     * In case you want to generate any file in the output path as a part of pre-build step, this plugin will likely
-     * remove those before the webpack build. In that case consider disabling the plugin, and instead use something like
-     * `del` (https://www.npmjs.com/package/del), or `rimraf` (https://www.npmjs.com/package/rimraf).
-     */
-    new CleanWebpackPlugin()
+    // Note that the usage of following plugin cleans the webpack output directory before build.
+    new CleanWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin()
   ]
 });
