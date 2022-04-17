ef3d9c317fb4adabcb5fce1951949dbd29eb15b1
ghiscoding <gbeaulac@gmail.com>
2022-04-17 15:47:34 -0400

    fix(server): Fix HttpExceptionFilter for graphqlException

--- server/src/shared/filters/http-exception.filter.ts
+++ server/src/shared/filters/http-exception.filter.ts
@@ -7,7 +7,7 @@ import {
 import { Response } from 'express';
 
 @Catch(HttpException)
-export class HttpExceptionFilter<T extends HttpException> implements ExceptionFilter {
+export class HttpExceptionFilter<T extends HttpException> implements ExceptionFilter<T> {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
@@ -16,12 +16,14 @@ export class HttpExceptionFilter<T extends HttpException> implements ExceptionFi
     const exceptionResponse = exception.getResponse();
     const error = typeof response === 'string' ? { message: exceptionResponse } : (exceptionResponse as Record<string, unknown>);
 
-    if (response && response.hasOwnProperty('status')) {
+    if (response && typeof response.status === 'function') {
       response.status(statusCode).json({
         ...error,
         path: request.url,
         timestamp: new Date().toISOString(),
       });
+    } else {
+      return exception;
     }
   }
 }
